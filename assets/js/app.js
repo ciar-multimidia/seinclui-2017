b7d7f4c742cb0aa58a164628d529facd66361ad7
anaflaviacador <aflaviah15@gmail.com>
2017-07-28 10:16:54 -0300

    Criação de layout

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+
+	// 1o passo do documento: Injetar os SVGs atraves de ajax.
+	var SvgsParaInjetar = $('img.injetar');
+	SVGInjector(SvgsParaInjetar);
+
+	
+
+});
