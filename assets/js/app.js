e8ed9d51eaf335164698493643cafdb813781560
vitaozim <victorhugocgodoi@gmail.com>
2017-08-28 17:11:16 -0300

    Favicon + carrousel de texto em andamento + correções
    Mini correções de acessibilidade em botões e SVGs
    CSS e JS do carrousel de texto em adamento
    Outros...

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,64 @@ jQuery(document).ready(function($) {
 		$(this).closest('.texto-video').toggleClass('video-destacado');
 	});
 
+	var slidersTxt = $('.slider-txt');
+	slidersTxt.each(function(index, el) {
+		var esseSlider = $(this);
+		var slideAtual = 1;
+		var botoes = esseSlider.find('button');
+		var inputRange = esseSlider.find('input[type="range"]');
+		var contagemSlide = esseSlider.find('p.contagem > span');
+		var textosSlides = esseSlider.find('.slider-items > p');
+		var contTexto = esseSlider.find('p.cont-texto');
+
+		function alterarSlide(indexNewSlide){
+			var indexNewSlideInt = parseInt(indexNewSlide);
+			if (indexNewSlideInt === 1) {
+				botoes.filter('.anterior').attr('disabled', 'disabled');
+			} else if(indexNewSlideInt === textosSlides.length){
+				botoes.filter('.proximo').attr('disabled', 'disabled');
+			} else{
+				botoes.removeAttr('disabled');
+			}
+
+			contagemSlide.text(indexNewSlideInt);
+
+			contTexto
+			.html(
+				textosSlides.eq(indexNewSlideInt-1).html()
+			)
+			.css('opacity', '0')
+			.stop().animate({'opacity': '1'}, 200);
+
+			inputRange.attr({
+				'value': indexNewSlideInt,
+				'aria-valuenow': indexNewSlideInt,
+				'aria-valuetext': 'Item '+indexNewSlideInt
+			}).val(indexNewSlideInt);
+
+			slideAtual = indexNewSlideInt;
+		}
+
+
+		botoes.on('click', function(event) {
+
+			var antOuProx;
+			if ($(this).hasClass('proximo') && slideAtual < textosSlides.length) {
+				antOuProx = slideAtual+1;
+			} else if($(this).hasClass('anterior') && slideAtual > 1){
+				antOuProx = slideAtual-1;
+			}
+
+			alterarSlide(antOuProx);
+
+		});
+
+		inputRange.on('input', function(event) {
+			var novoValor = $(this).val();
+			alterarSlide(novoValor);
+
+		});
+
+	});
+
 });
