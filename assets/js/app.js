71dd0b66421dd96cadb171811b79ef52d51f3120
vitaozim <victorhugocgodoi@gmail.com>
2017-08-31 10:39:45 -0300

    Continuidade do slider de txt
    Agora o slider rola. Cada item do slider está um do lado do outro atraves de nowrap. CUIDADO COM O GAP DO DIB.

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,18 +15,18 @@ jQuery(document).ready(function($) {
 		var botoes = esseSlider.find('button');
 		var inputRange = esseSlider.find('input[type="range"]');
 		var contagemSlide = esseSlider.find('p.contagem > span');
-		var textosSlides = esseSlider.find('.slider-items > p');
-		var contTexto = esseSlider.find('p.cont-texto');
+		var textosSlides = esseSlider.find('.slider-items > div.scroller > p');
+		// var contTexto = esseSlider.find('p.cont-texto');
 		var imgSlides = esseSlider.find('.imagens > figure');
 		var slideAtual = 1;
-		// var slideAtual = parseInt(contagemSlide.text());
-
-
+		var qtdeItens = textosSlides.length > 0 ? textosSlides.length : imgSlides.length > 0 ? imgSlides.length : undefined;
+		
+		
 		function alterarSlide(indexNewSlide){
 			
 			if (indexNewSlide === 1) {
 				botoes.filter('.anterior').attr('disabled', 'disabled');
-			} else if(indexNewSlide === textosSlides.length){
+			} else if(indexNewSlide === qtdeItens){
 				botoes.filter('.proximo').attr('disabled', 'disabled');
 			} else{
 				botoes.removeAttr('disabled');
@@ -40,13 +40,26 @@ jQuery(document).ready(function($) {
 				'aria-valuetext': 'Item '+indexNewSlide
 			}).val(indexNewSlide);
 
-			if (contTexto.length > 0 && textosSlides.length > 0) {
-				contTexto
-				.html(
-					textosSlides.eq(indexNewSlide-1).html()
-				)
-				.css('opacity', '0')
-				.stop().animate({'opacity': '1'}, 200);
+			// if (contTexto.length > 0 && textosSlides.length > 0) {
+			// 	contTexto
+			// 	.html(
+			// 		textosSlides.eq(indexNewSlide-1).html()
+			// 	)
+			// 	.css('opacity', '0')
+			// 	.stop().animate({'opacity': '1'}, 200);
+			// }
+
+			if (textosSlides.length > 0) {
+
+
+				textosSlides
+				.attr('aria-hidden', 'true')
+				.eq(indexNewSlide-1)
+				.removeAttr('aria-hidden')
+				.end()
+				.parent()
+				.css('transform', 'translateX(-'+660*(indexNewSlide-1)+'px)');
+				
 			}
 
 			if (imgSlides.length > 0) {
@@ -66,9 +79,9 @@ jQuery(document).ready(function($) {
 		botoes.on('click', function(event) {
 
 			var antOuProx;
-			if ($(this).hasClass('proximo')) {
+			if ($(this).hasClass('proximo') && slideAtual < qtdeItens) {
 				antOuProx = parseInt(slideAtual+1);
-			} else if($(this).hasClass('anterior')){
+			} else if($(this).hasClass('anterior') && slideAtual > 0){
 				antOuProx = parseInt(slideAtual-1);
 			}
 
